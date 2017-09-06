c8911633fc3aaa08ceb2f95a77ac03ef506f29d5
nieyan <260715780@qq.com>
2017-09-06 16:08:55 +0800

    news

--- src/components/news/news.vue
+++ src/components/news/news.vue
@@ -1,21 +1,30 @@
 <template>
   <div>
-    <common>
-      <div>news</div>
+    <common title="新闻资讯">
+      <component :listdetailId="listdetailId" :is="comp" @itemClickFun="itemClickFun" @backFun="backFun"></component>
     </common>
   </div>
 </template>
 <script>
-import common from '@/components/common/common';
-export default {
-  components:{common},
-  data(){
-      return{
-
+  import common from '@/components/common/common';
+  import list from '@/components/news/list';
+  import listitem from '@/components/news/listitem';
+  export default {
+    components: { common,list, listitem},
+    data() {
+      return {
+        comp: list,
+        listdetailId:""
       }
+    },
+    methods:{
+      itemClickFun(index){
+          this.listdetailId=index;
+        this.comp = listitem;
+      },
+      backFun(){
+        this.comp = list;
+      }
+    }
   }
-}
 </script>
-<style lang="scss" scoped>
-@import './news.scss';
-</style>
